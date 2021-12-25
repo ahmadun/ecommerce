1345dc8c1118f05fdb7300d10e69d135ed360efb
ahmadun <ahmadun.jambi@gmail.com>
2021-12-25 14:04:54 +0700

    fix carousal

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -54,7 +54,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h1 class="h1">Portal Berita Codeignitera</h1>
+					<h1 class="h1">Portal Berita Codeigniter</h1>
 				</div>
 			</div>
 		</div>
@@ -63,71 +63,169 @@
 	<div class="container">
 		<div class="row">
 
-			<div class="large-12 columns">
-				<div class="owl-carousel">
+			<div class="owl-carousel">
 
-					<div class="item card spacer">
+				<div class="item">
+					<div class="card spacer">
 						<div class="card-body pad">
-							<div style="height: 100%;">
-								<div class="wrap-img">
-									<img class="img-sale" src="<?= base_url('img/celana.jpg') ?>" alt="Image">
-									<div class="buy-el">
-										<div class="opt-buy" style="background-color: aqua;">A</div>
-										<div class="opt-buy" style="background-color: blueviolet;">B</div>
-										<div class="opt-buy" style="background-color: chartreuse;">C</div>
-									</div>
+							<div class="wrap-img">
+								<img class="img-sale" src="<?= base_url('img/celana.jpg') ?>" alt="Image">
+								<div class="buy-el">
+									<div class="opt-buy" style="background-color: aqua;">A</div>
+									<div class="opt-buy" style="background-color: blueviolet;">B</div>
+									<div class="opt-buy" style="background-color: chartreuse;">C</div>
 								</div>
-								<div class="title-product">Celana Straight Fit Pants Basic Celana Straight Fit Pants Basic</div>
-								<div class="prc-product">
-									<b class="prc-ori">120.000</b> <b class="prc-offer">130.000</b>
-								</div>
+							</div>
+							<div class="title-product">Celana Straight Fit Pants Basic Celana Straight Fit Pants Basic</div>
+							<div class="prc-product">
+								<b class="prc-ori">120.000</b> <b class="prc-offer">130.000</b>
 							</div>
 						</div>
 					</div>
-
-					<div class="item card spacer">
-						<div class="card-body pad">
-							<div style="height: 100%;">
-								<div class="wrap-img">
-									<img class="img-sale" src="<?= base_url('img/baju.jpg') ?>" alt="Image">
-									<div class="buy-el">
-										<div class="opt-buy" style="background-color: aqua;">A</div>
-										<div class="opt-buy" style="background-color: blueviolet;">B</div>
-										<div class="opt-buy" style="background-color: chartreuse;">C</div>
-									</div>
-								</div>
-								<div class="title-product">Celana Straight Fit Pants Basic Celana Straight Fit Pants Basic</div>
-								<div class="prc-product">
-									<b class="prc-ori">120.000</b> <b class="prc-offer">130.000</b>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="item card spacer">
-						<div class="card-body pad">
-							<div style="height: 100%;">
-								<div class="wrap-img">
-									<img class="img-sale" src="<?= base_url('img/hijab.jpg') ?>" alt="Image">
-									<div class="buy-el">
-										<div class="opt-buy" style="background-color: aqua;">A</div>
-										<div class="opt-buy" style="background-color: blueviolet;">B</div>
-										<div class="opt-buy" style="background-color: chartreuse;">C</div>
-									</div>
-								</div>
-								<div class="title-product">Celana Straight Fit Pants Basic Celana Straight Fit Pants Basic</div>
-								<div class="prc-product">
-									<b class="prc-ori">120.000</b> <b class="prc-offer">130.000</b>
-								</div>
-							</div>
-						</div>
-					</div>
-
-
 				</div>
+				<div class="item">
+					<div class="card spacer">
+						<div class="card-body pad">
+							<div class="wrap-img">
+								<img class="img-sale" src="<?= base_url('img/baju.jpg') ?>" alt="Image">
+								<div class="buy-el">
+									<div class="opt-buy" style="background-color: aqua;">A</div>
+									<div class="opt-buy" style="background-color: blueviolet;">B</div>
+									<div class="opt-buy" style="background-color: chartreuse;">C</div>
+								</div>
+							</div>
+							<div class="title-product">Celana Straight Fit Pants Basic Celana Straight Fit Pants Basic</div>
+							<div class="prc-product">
+								<b class="prc-ori">120.000</b> <b class="prc-offer">130.000</b>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="item">
+					<div class="card spacer">
+						<div class="card-body pad">
+							<div class="wrap-img">
+								<img class="img-sale" src="<?= base_url('img/baju.jpg') ?>" alt="Image">
+								<div class="buy-el">
+									<div class="opt-buy" style="background-color: aqua;">A</div>
+									<div class="opt-buy" style="background-color: blueviolet;">B</div>
+									<div class="opt-buy" style="background-color: chartreuse;">C</div>
+								</div>
+							</div>
+							<div class="title-product">Celana Straight Fit Pants Basic Celana Straight Fit Pants Basic</div>
+							<div class="prc-product">
+								<b class="prc-ori">120.000</b> <b class="prc-offer">130.000</b>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="item">
+					<div class="card spacer">
+						<div class="card-body pad">
+							<div class="wrap-img">
+								<img class="img-sale" src="<?= base_url('img/baju.jpg') ?>" alt="Image">
+								<div class="buy-el">
+									<div class="opt-buy" style="background-color: aqua;">A</div>
+									<div class="opt-buy" style="background-color: blueviolet;">B</div>
+									<div class="opt-buy" style="background-color: chartreuse;">C</div>
+								</div>
+							</div>
+							<div class="title-product">Celana Straight Fit Pants Basic Celana Straight Fit Pants Basic</div>
+							<div class="prc-product">
+								<b class="prc-ori">120.000</b> <b class="prc-offer">130.000</b>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="item">
+					<div class="card spacer">
+						<div class="card-body pad">
+							<div class="wrap-img">
+								<img class="img-sale" src="<?= base_url('img/baju.jpg') ?>" alt="Image">
+								<div class="buy-el">
+									<div class="opt-buy" style="background-color: aqua;">A</div>
+									<div class="opt-buy" style="background-color: blueviolet;">B</div>
+									<div class="opt-buy" style="background-color: chartreuse;">C</div>
+								</div>
+							</div>
+							<div class="title-product">Celana Straight Fit Pants Basic Celana Straight Fit Pants Basic</div>
+							<div class="prc-product">
+								<b class="prc-ori">120.000</b> <b class="prc-offer">130.000</b>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="item">
+					<div class="card spacer">
+						<div class="card-body pad">
+							<div class="wrap-img">
+								<img class="img-sale" src="<?= base_url('img/baju.jpg') ?>" alt="Image">
+								<div class="buy-el">
+									<div class="opt-buy" style="background-color: aqua;">A</div>
+									<div class="opt-buy" style="background-color: blueviolet;">B</div>
+									<div class="opt-buy" style="background-color: chartreuse;">C</div>
+								</div>
+							</div>
+							<div class="title-product">Celana Straight Fit Pants Basic Celana Straight Fit Pants Basic</div>
+							<div class="prc-product">
+								<b class="prc-ori">120.000</b> <b class="prc-offer">130.000</b>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
 
 			</div>
 		</div>
+
+
+		<div class="row row-big-offer">
+			<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+				<div class="carousel-indicators">
+					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+				</div>
+				<div class="carousel-inner big-offer">
+					<div class="carousel-item active">
+						<img src="<?= base_url('img/celana.jpg') ?>" class="d-block w-100" alt="...">
+					</div>
+					<div class="carousel-item">
+						<img src="<?= base_url('img/celana.jpg') ?>" class="d-block w-100" alt="...">
+					</div>
+					<div class="carousel-item">
+						<img src="<?= base_url('img/celana.jpg') ?>" class="d-block w-100" alt="...">
+					</div>
+				</div>
+				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Previous</span>
+				</button>
+				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Next</span>
+				</button>
+			</div>
+		</div>
+
+
 	</div>
+
+
+
+
+
+
+
+
 
 	<footer class="jumbotron jumbotron-fluid mt-5 mb-0">
 		<div class="container text-center">Copyright &copy <?= Date('Y') ?> CI News</div>
@@ -140,6 +238,9 @@
 		$(document).ready(function() {
 			var owl = $('.owl-carousel');
 			owl.owlCarousel({
+				autoWidth: true,
+				merge: true,
+				margin: 10,
 				responsive: {
 					0: {
 						items: 1
